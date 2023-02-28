68dc2f9f3050acb69ca831500288a32c1f26c957
Lucas-Debiais <lucas.debiais11@gmail.com>
2023-02-28 12:25:25 +0100

    Feat: pwa

--- vite.config.js
+++ vite.config.js
@@ -13,26 +13,26 @@ export default defineConfig({
         mkcert(),
         VitePWA({
             registerType: 'autoUpdate',
-            includeAssets: ['public/icon.svg, public/icon.png'],
+            includeAssets: ['/icon.svg, /icon.png'],
             manifest: {
                 name: 'WC Locator',
                 description: 'Les toilettes publiques des grandes villes de france',
                 theme_color: '#ffffff',
                 icons: [
                     {
-                        src: 'public/pwa-64x64.svg',
+                        src: '/pwa-64x64.svg',
                         sizes: '64x64',
                         type: 'image/svg+xml',
                         purpose: "any maskable"
                     },
                     {
-                        src: 'public/pwa-192x192.png',
+                        src: '/pwa-192x192.png',
                         sizes: '192x192',
                         type: 'image/png',
                         purpose: "any maskable"
                     },
                     {
-                        src: 'public/pwa-512x512.png',
+                        src: '/pwa-512x512.png',
                         sizes: '512x512',
                         type: 'image/png',
                         purpose: "any maskable"
